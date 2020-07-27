b7bac8184b5491ac48dfadc93348ec3a4915ef91
Electroblob77 <35599699+Electroblob77@users.noreply.github.com>
2020-07-27 16:41:57 +0100

    Update version number

--- CREDITS.md
+++ CREDITS.md
@@ -1,7 +1,7 @@
 # Credits
 
 Electroblob's Wizardry: Twilight Forest Spell Pack  
-Version 1.0.1  
+Version 1.0.2  
 For Minecraft 1.12.2, Twilight Forest 3.10 and Wizardry 4.2
 
 Designed, coded and textured by Electroblob
--- src/main/java/electroblob/tfspellpack/TFSpellPack.java
+++ src/main/java/electroblob/tfspellpack/TFSpellPack.java
@@ -25,7 +25,7 @@ public class TFSpellPack {
 
     public static final String MODID = "tfspellpack";
     public static final String NAME = "Electroblob's Wizardry: Twilight Forest Spell Pack";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static Logger logger;
 
